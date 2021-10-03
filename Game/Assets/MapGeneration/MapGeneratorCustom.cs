b7765c1150a7142c2b6b6ec54f8d78fc8c44a457
hasKhan-cloud <78858679+hasKhan-cloud@users.noreply.github.com>
2021-10-03 20:11:04 +0100

    pre-obstacles 03.11.21

--- Game/Assets/MapGeneration/MapGeneratorCustom.cs
+++ Game/Assets/MapGeneration/MapGeneratorCustom.cs
@@ -2,107 +2,100 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MapGeneratorCustom : MonoBehaviour
+public class MapGeneratorCustom: MonoBehaviour
 {
     Dictionary<int, GameObject> tileset;
-    Dictionary<int, GameObject> tilegroups;
-    Dictionary<GameObject, int> heuristic; //used in gridmap for pathfinding.
-    public GameObject tile_1; //used to pick prefabs for tileset.
-    public GameObject tile_2;
-    public GameObject tile_3;
-    public GameObject tile_4;
-
-    int map_width = 48; //Creates the map size
-    int map_height = 27; //use 16:9 ratio for width:height
-
-    List<List<int>> noise_grid = new List<List<int>>(); //creating empty 2D array that takes lists.
+    Dictionary<int, GameObject> tile_groups;
+    public GameObject tileset_0;
+    public GameObject tileset_3;
+    public GameObject tileset_13;
+    public GameObject tileset_27;
+    int map_width = 48;
+    int map_height = 27;
+    List<List<int>> noise_grid = new List<List<int>>();
     List<List<GameObject>> tile_grid = new List<List<GameObject>>();
+    // recommend 4 to 20
+    float magnification = 14.0f;
+    int x_offset = 0; // <- +>
+    int y_offset = 0; // v- +^
 
-    public float magnification = 7.0f;
-    public int x_offset = 0;
-    public int y_offset = 0;
-    //used to randomise the map, offset moves the map, magnification is used to zoom in.
-
-
-    // Start is called before the first frame update
     void Start()
     {
+    
         x_offset = Random.Range(0, 9999); //randomises the map on start up
         y_offset = Random.Range(0, 9999);
-        Create_tileset();
-        Generate_Map();
+        CreateTileset();
         CreateTileGroups();
+        GenerateMap();
+        transform.Rotate(0,0,0); //for gravity it is placed on the x-plane.
 
     }
-
-    void Create_tileset() //collects tile prefabs and assigns them integers for ease of use.
+    void CreateTileset()
     {
+        /** Collect and assign ID codes to the tile prefabs, for ease of access.
+            Best ordered to match land elevation. **/
         tileset = new Dictionary<int, GameObject>();
-        tileset.Add(0, tile_1);
-        tileset.Add(1, tile_2);
-        tileset.Add(2, tile_3);
-        tileset.Add(4, tile_4);
-        heuristic = new Dictionary<GameObject, int>();
-        heuristic.Add(tile_1, 4 ); //heuristic depends on tile.
-        heuristic.Add(tile_2, 3 );
-        heuristic.Add(tile_3, 2 );
-        heuristic.Add(tile_4, 1 );
-
+        tileset.Add(0, tileset_0);
+        tileset.Add(1, tileset_3);
+        tileset.Add(2, tileset_13);
+        tileset.Add(3, tileset_27);
     }
-
-    void Generate_Map()
+    void CreateTileGroups()
     {
-        for (int x = 0; x < map_width; x++ )
+        /** Create empty gameobjects for grouping tiles of the same type, ie
+            forest tiles **/
+        tile_groups = new Dictionary<int, GameObject>();
+        foreach (KeyValuePair<int, GameObject> prefab_pair in tileset) //value refers to whether key or value of prefab-pair
         {
-            noise_grid.Add(new List<int>()); //create lists for the amount of collumns
-            tile_grid.Add(new List<GameObject>());
-
-            for(int y = 0; y < map_height; y++) //creates a value for each row within a collumn
-            {
-                int tile_id = GetIdUsingPerlinNoise(x, y);
-                noise_grid[x].Add(tile_id); 
-                CreateTile(tile_id, x, y); //adds tile to tile_grid
-            }
-
-
+            GameObject tile_group = new GameObject(prefab_pair.Value.name);
+            tile_group.transform.parent = gameObject.transform;
+            tile_group.transform.localPosition = new Vector3(0, 0, 0);
+            tile_groups.Add(prefab_pair.Key, tile_group);
         }
     }
-
-    int GetIdUsingPerlinNoise(int x , int y)
+    void GenerateMap()
     {
-        float raw_perlin = Mathf.PerlinNoise( //creates pseudorandom 2d map
-            (x-x_offset)/magnification,
-            (y-y_offset)/magnification
-            );
-        float clamp_perlin = Mathf.Clamp01(raw_perlin);
-        float scaled_perlin = clamp_perlin * tileset.Count; //changes range from 0-3
+        /** Generate a 2D grid using the Perlin noise fuction, storing it as
+            both raw ID values and tile gameobjects **/
+        for (int x = 0; x < map_width; x++)
+        {
+            noise_grid.Add(new List<int>());
+            tile_grid.Add(new List<GameObject>());
+            for (int y = 0; y < map_height; y++)
+            {
+                int tile_id = GetIdUsingPerlin(x, y);
+                noise_grid[x].Add(tile_id);
+                CreateTile(tile_id, x, y);
+            }
+        }
+    }
+    int GetIdUsingPerlin(int x, int y)
+    {
+        /** Using a grid coordinate input, generate a Perlin noise value to be
+            converted into a tile ID code. Rescale the normalised Perlin value
+            to the number of tiles available. **/
+        float raw_perlin = Mathf.PerlinNoise(
+            (x - x_offset) / magnification,
+            (y - y_offset) / magnification
+        );
+        float clamp_perlin = Mathf.Clamp01(raw_perlin); // Thanks: youtu.be/qNZ-0-7WuS8&lc=UgyoLWkYZxyp1nNc4f94AaABAg
+        float scaled_perlin = clamp_perlin * tileset.Count;
+        // Replaced 4 with tileset.Count to make adding tiles easier
         if (scaled_perlin == tileset.Count)
         {
             scaled_perlin = (tileset.Count - 1);
         }
         return Mathf.FloorToInt(scaled_perlin);
     }
-
     void CreateTile(int tile_id, int x, int y)
     {
-        GameObject tile_prefab = tileset[tile_id]; //gets the tile from dictionary
-        GameObject tile_group = tilegroups[tile_id];
+        /** Creates a new tile using the type id code, group it with common
+            tiles, set it's position and store the gameobject. **/
+        GameObject tile_prefab = tileset[tile_id];
+        GameObject tile_group = tile_groups[tile_id];
         GameObject tile = Instantiate(tile_prefab, tile_group.transform);
         tile.name = string.Format("tile_x{0}_y{1}", x, y);
         tile.transform.localPosition = new Vector3(x, y, 0);
         tile_grid[x].Add(tile);
     }
-
-    void CreateTileGroups() //groups tiles of the same type, used in cellular automata.
-    {
-        tilegroups = new Dictionary<int, GameObject>();
-        foreach (KeyValuePair<int, GameObject> prefab_pair in tileset)
-        {
-            GameObject tile_group = new GameObject(prefab_pair.Value.name);
-            tile_group.transform.parent = gameObject.transform;
-            tile_group.transform.localPosition = new Vector3(0, 0, 0);
-            tilegroups.Add(prefab_pair.Key, tile_group);
-        }
-    }
-
 }
